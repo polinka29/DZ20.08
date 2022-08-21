c82905f0d29eb72324a6ba28d1af49596f626549
Polina <polinka29enp@gmail.com>
2022-08-21 22:03:43 +0300

    Zadacha36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+
+int [] array = new int [5];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+     array[i] = new Random().Next(10);
+    if(i %2 > 0) sum = sum + array[i];
+    {
+     Console.WriteLine(array[i]);
+    }
+}
+Console.WriteLine($"сумма элементов чисел на нечетных позициях: =  {sum}");
+
